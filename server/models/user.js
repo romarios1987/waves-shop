f0960f3073a14223ef8203446dc89c0d8f4cf5ae
Roman Batiuk <roman.batiuk@cyberchisel.com>
2019-07-03 16:55:38 +0300

    User model & registering users

---
+++ server/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 5,
+    },
+    first_name: {
+        type: String,
+        required: true,
+        maxLength: 100
+    },
+    last_name: {
+        type: String,
+        required: true,
+        maxLength: 100
+    },
+    cart: {
+        type: Array,
+        default: []
+    },
+    history: {
+        type: Array,
+        default: []
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    token: {
+        type: String
+    }
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+
+module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,43 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true});
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+// Models
+const {User} = require('./models/user');
+
+
+/***
+ * USERS
+ */
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+
+
+    user.save((err, doc) => {
+        if (err) return res.json({success: false, err});
+        res.status(200).json({
+            success: true,
+            user_data: doc
+        })
+
+
+    })
+});
+
+
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
