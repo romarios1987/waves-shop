6b4317e393523fba59a2a620644130680921d217
Roman Batiuk <roman.batiuk@cyberchisel.com>
2019-07-09 18:22:28 +0300

    Auth route & Logout

--- server/models/user.js
+++ server/models/user.js
@@ -94,6 +94,20 @@ userSchema.methods.generateToken = function (cb) {
 
 };
 
+
+userSchema.statics.findByToken = function (token, cb) {
+
+    // this === user
+
+    jwt.verify(token, process.env.SECRET, (err, decode) => {
+        this.findOne({"_id": decode, "token": token}, (err, user) => {
+            if (err) return cb(err);
+            cb(null, user)
+        })
+    })
+
+};
+
 const User = mongoose.model('User', userSchema);
 
 
--- server/server.js
+++ server/server.js
@@ -19,9 +19,45 @@ app.use(cookieParser());
 const {User} = require('./models/user');
 
 
-/***
- * USERS
- */
+// Middleware
+const {auth} = require('./middleware/auth');
+
+
+/**********************************
+ * ***************USERS***********
+ ***********************************/
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        // user: req.user
+
+        isAdmin: req.user.role !== 0,
+        iAuth: true,
+        email: req.user.first_name,
+        name: req.user.last_name,
+        role: req.user.role,
+        cart: req.user.cart,
+        history: req.user.history
+    })
+});
+
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate(
+        {_id: req.user._id},
+        {token: ''},
+        (err, doc) => {
+            if (err) return res.json({success: false, err});
+
+            return res.status(200).send({
+                success: true,
+                message: 'Logout Success'
+            })
+        }
+    )
+});
+
+
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
 
@@ -29,7 +65,7 @@ app.post('/api/users/register', (req, res) => {
         if (err) return res.json({success: false, err});
         res.status(200).json({
             success: true,
-            user_data: doc
+            // user_data: doc
         })
 
 
@@ -48,19 +84,15 @@ app.post('/api/users/login', (req, res) => {
             if (!isMatch) return res.json({loginSuccess: false, message: 'Wrong password'});
 
 
-
-
             // generate token
             user.generateToken((err, user) => {
-                if(err) return res.status(400).send(err);
+                if (err) return res.status(400).send(err);
 
                 res.cookie('x_auth', user.token).status(200).json({loginSuccess: true, message: 'Login Success'})
 
             })
         })
     })
-
-
 
 
 });
