ce8ceb5f6c7a78276202c2590a4e6a0e087e17c6
Roman Batiuk <roman.batiuk@cyberchisel.com>
2019-07-10 11:21:49 +0300

    Products (add, get_by_id, get_by_order)

--- server/server.js
+++ server/server.js
@@ -19,11 +19,86 @@ app.use(cookieParser());
 const {User} = require('./models/user');
 const {Brand} = require('./models/brand');
 const {Wood} = require('./models/wood');
+const {Product} = require('./models/product');
 
 
 // Middleware
 const {auth} = require('./middleware/auth');
 const {admin} = require('./middleware/admin');
+
+
+/**********************************
+ * ***************PRODUCTS***********
+ ***********************************/
+
+
+
+// by ARRIVAL
+// /articles?sortBy=createdAt&order=desc&limit=2
+
+// by SELL
+// /articles?sortBy=sold&order=desc&limit=50
+
+app.get('/api/product/articles', (req, res) => {
+
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+
+    Product
+        .find()
+        .populate('brand')
+        .populate('wood')
+        .sort([sortBy, order])
+        .limit(limit)
+        .exec((err, articles) => {
+            if (err) return res.status(400).send(err);
+
+            res.send(articles)
+        })
+
+});
+
+
+// /api/product/articles_by_id?id=5d2591abbefe1e4517ff9e5a,5d2597ac42dbc65325351b1a&type=array
+
+app.get('/api/product/articles_by_id', (req, res) => {
+    let type = req.query.type;
+
+    let items = req.query.id;
+
+
+    if (type === 'array') {
+        let ids = req.query.id.split(',');
+        items = [];
+        items = ids.map((item) => {
+            return mongoose.Types.ObjectId(item)
+        })
+    }
+
+    Product
+        .find({'_id': {$in: items}})
+        .populate('brand')
+        .populate('wood')
+        .exec((err, docs) => {
+            return res.status(200).send(docs)
+        })
+});
+
+
+app.post('/api/product/article', auth, admin, (req, res) => {
+    const product = new Product(req.body);
+
+    product.save((err, doc) => {
+        if (err) return res.json({success: false, err});
+
+        res.status(200).json({
+            success: true,
+            article: doc
+        })
+    })
+});
 
 
 /**********************************
